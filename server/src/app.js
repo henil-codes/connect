950e68221be16c37f8649bd89f6939c29e68b4d2
Jayant Nepal <nepaljayant51@gmail.com>
2025-10-24 17:07:51 -0400

    Profile Page Endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -13,17 +12,19 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+cors({
+origin: "http://localhost:5173",
+credentials: true,
+})
 );
 app.use(cookieParser())
-
 /* IMPORT ROUTES */
 import authRouter from "./routes/auth.js";
-
+import userRouter from "./routes/users.js";
 /* ROUTES DEFINITION */
 app.use("/auth", authRouter);
-
+app.use("/users", userRouter);
+app.get("/", (req, res) => {
+res.send("Home Page");
+});
 export default app;
