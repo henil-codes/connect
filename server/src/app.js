005a7638b66160e39b11d4c3ee4c535f42874f2f
HENILKUMAR PRAFULCHANDRA PATEL <85500288+henil-codes@users.noreply.github.com>
2025-10-25 18:00:07 -0400

    fixed backend dependency issue

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -9,8 +8,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
 cors({
 origin: "http://localhost:5173",
